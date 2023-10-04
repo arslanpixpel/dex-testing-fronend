5adf0f4e344ef27a6a5d7de599791474b14ee572
Danylo Oliinyk <dee4is.nft@ud.me>
2023-10-04 15:39:13 +0300

    Update config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -8,11 +8,11 @@ export const addressesConfig = {
     EXPLORER_URL: "https://goerli.etherscan.io/tx/",
   },
   2221: {
-    ROOT_MANAGER_ADDRESS: "0x8469d2a9910251d990bCdb73fF61C4fBfBdBc8Ce",
+    ROOT_MANAGER_ADDRESS: "0x774179419aE9dE44085abBf33ef4B442232BCD57",
     ERC20_VAULT_TYPE: "0x8ae85d849167ff996c04040c44924fd364217285e4cad818292c7ac37c0a345b",
-    BRIDGE_MANAGER_INDEX: "4602",
+    BRIDGE_MANAGER_INDEX: "6852",
     BRIDGE_MANAGER_SUBINDEX: "0",
-    API_URL: "https//pixp.dee4is.lol/api/",
+    API_URL: "<YOUR_API_URL>",
     EXPLORER_URL: "https://explorer.testnet.kava.io/tx/",
   },
 };
