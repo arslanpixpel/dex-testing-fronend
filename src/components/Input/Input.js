a3687bdb4662d4bb041d496ccf68af7cded3f5aa
arslanpixpel <arslankhan@pixpel.io>
2024-01-01 14:58:33 +0500

    Update: Input e remove and 0 added in input fields

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,9 +2,14 @@ import { useFormContext } from "react-hook-form";
 
 // Input connected to form Provider
 const Input = ({ name, type = "text", className, validation, ...restProps }) => {
+  const handleKeyDown = (e) => {
+    // Check if the pressed key is 'e' or 'E' and prevent the input
+    if (e.key === 'e' || e.key === 'E') {
+      e.preventDefault();
+    }
+  };
   const { register } = useFormContext();
-
-  return <input type={type} className={className} {...register(name, validation)} {...restProps} />;
+  return <input type={type} placeholder="0" onKeyDown={handleKeyDown} className={className} {...register(name, validation)} {...restProps} />;
 };
 
 export default Input;
